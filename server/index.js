3175bc19919fa37c96e6c391ed9cce6507704269
Andy Parinas <andy.parinas@gmail.com>
2019-02-12 10:43:05 +1000

    Refactor Server.js for Backend api

---
+++ server/index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const next = require('next')
+
+const routes = require('../routes');
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = routes.getRequestHandler(app);
+
+const secretData = [
+    { title: 'Secret Data 1', description: 'Build hyperdrive'},
+    { title: 'Secret Data 2', description: 'Search for Dark Matter'}
+]
+
+app.prepare()
+.then(() => {
+  const server = express()
+
+    server.get('/api/v1/secret', (req, res) => {
+        return res.json(secretData);
+    })
+
+    server.get('*', (req, res) => {
+        return handle(req, res)
+    })
+
+    server.use(handle).listen(3000, (err) => {
+        if (err) throw err
+        console.log('> Ready on http://localhost:3000')
+    })
+})
+.catch((ex) => {
+  console.error(ex.stack)
+  process.exit(1)
+})
